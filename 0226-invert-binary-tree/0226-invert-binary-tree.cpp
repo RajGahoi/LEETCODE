a22492fd7012a5359fddc52b53c5f3215f1a1750
Raj Gahoi <gahoiraj01@gmail.com>
2026-07-14 13:14:50 +0530

    Time: 0 ms (100%), Space: 12.5 MB (61.36%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -0,0 +1,10 @@
+ class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+        swap(root->right,root->left);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
