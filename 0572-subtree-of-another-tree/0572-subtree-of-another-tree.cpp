ed80d58dfdf674975e2b0c2be9c03317260bdc18
Raj Gahoi <gahoiraj01@gmail.com>
2026-07-27 03:28:19 +0530

    Time: 0 ms (100%), Space: 29.1 MB (39.27%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkif(TreeNode* root, TreeNode* subRoot) {
+        if (!root && !subRoot) return true;
+        if (!root || !subRoot) return false;
+        if (root->val != subRoot->val) return false;
+
+        return checkif(root->left, subRoot->left) &&
+               checkif(root->right, subRoot->right);
+    }
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!subRoot) return true;
+        if (!root) return false;
+
+        if (checkif(root, subRoot))
+            return true;
+
+        return isSubtree(root->left, subRoot) ||
+               isSubtree(root->right, subRoot);
+    }
+};
