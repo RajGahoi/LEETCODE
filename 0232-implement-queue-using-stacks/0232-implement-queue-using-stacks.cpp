cf9a43580cf6806e8a33ac16de69afe00db09e5c
Raj Gahoi <gahoiraj01@gmail.com>
2026-09-14 10:27:54 +0530

    Time: 0 ms (100%), Space: 10.1 MB (47.2%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,39 @@
+class MyQueue {
+    stack<int>inStack;
+    stack<int>outStack;
+public:
+    MyQueue() {
+        
+    }
+    void push(int x) {
+        inStack.push(x);
+        
+    }
+    
+    int pop() {
+        if(outStack.empty()){
+            while(!inStack.empty()){
+                outStack.push(inStack.top());
+                inStack.pop();
+            }
+        }
+        int ans = outStack.top();
+        outStack.pop();
+        return ans;
+    }
+    
+    int peek() {
+        if(outStack.empty()){
+             while (!inStack.empty()) {
+                outStack.push(inStack.top());
+                inStack.pop();
+            }
+        }
+
+        return outStack.top();
+    }
+    
+    bool empty() {
+        return inStack.empty() && outStack.empty();
+    }
+};
